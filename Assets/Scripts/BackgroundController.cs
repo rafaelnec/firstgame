2e30a3a9d6c91a7163e1a71e92f5ed761d238296
Rafael Moraes <rafaelnec@gmail.com>
2025-10-30 20:24:10 -0700

    chore: upate bakg script

--- Assets/Scripts/BackgroundController.cs
+++ Assets/Scripts/BackgroundController.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class BackgroundController : MonoBehaviour
 {
-    public float screenPosition = -0.53f;
+    public float screenPosition = -0.35f;
 
     [SerializeField] private float scrollSpeed = 1f;
     private Vector3 startPosition;
